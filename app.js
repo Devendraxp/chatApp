a3759d1594b709cdea51ab9bc95f805f49ad65b5
devendraxp <dnowtech@gmail.com>
2025-01-11 16:11:22 +0530

    added ejs files

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 import express from 'express';
+import ejsMate from 'ejs-mate';
+import path from 'path';
+import ejs from 'ejs';
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
 const PORT = 3000;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(path.dirname(new URL(import.meta.url).pathname), "views"));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
+    res.send('Root is working !!!');
+});
+app.get('/register', (req, res) => {
+    res.render("user/registerEmail");
+});
 
 
 app.listen(PORT, () => {
