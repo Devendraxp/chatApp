2f9e327320e943f54402ccdfe075c03c13ee0feb
devendraxp <dnowtech@gmail.com>
2025-01-11 13:26:39 +0530

    added files

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from 'express';
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    });
+
+
+app.listen(PORT, () => {
+    console.log('Server is running on port 3000');
+    });
